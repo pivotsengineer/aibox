c6674df41822c7228a9e82f6db4d60ec78884f51
Sergei Sergienko <pivotsengineer@pivotsdoo.com>
2024-08-13 05:55:31 +0200

    segment

--- sockets.py
+++ sockets.py
@@ -8,37 +8,27 @@ app = Flask(__name__)
 def generate_frames():
     while True:
 
-        command = ['libcamera-vid', '--output', 'frame.mjpeg', '--width', '640', '--height', '480', '--codec', 'mjpeg', '--timeout', '50']
+        # Capture a single frame using libcamera
+        command = ['libcamera-still', '-o', 'frame.jpg', '-t', '0', '--width', '640', '--height', '480', '--segment', '1']
         subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        # Read the MJPEG stream
-        with open('frame.mjpeg', 'rb') as f:
-            frame = f.read()
-
-        yield (b'--frame\r\n'
-               b'Content-Type: video/mjpeg\r\n\r\n' + frame + b'\r\n')
-
-        # Capture a single frame using libcamera
-        # command = ['libcamera-still', '-o', 'frame.jpg', '-t' '50' '--width' '640' '--height' '480']
-        # subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
         # # Read the captured frame
-        # frame = cv2.imread('frame.jpg')
+        frame = cv2.imread('frame.jpg')
 
-        # if frame is None:
-        #     print('frame is None')
-        #     continue
+        if frame is None:
+            print('frame is None')
+            continue
 
         # # Resize the frame
-        # frame = cv2.resize(frame, (640, 480))  # Adjust the resolution as needed
+        frame = cv2.resize(frame, (640, 480))  # Adjust the resolution as needed
 
         # # Encode the frame in JPEG format
-        # _, buffer = cv2.imencode('.jpg', frame)
-        # frame = buffer.tobytes()
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
 
         # # Yield the frame to the response
-        # yield (b'--frame\r\n'
-        #        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
