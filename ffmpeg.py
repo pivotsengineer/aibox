8024eb06717a264796ddee56f41011f516913719
Sergei Sergienko <pivotsengineer@pivotsdoo.com>
2024-08-13 08:05:09 +0200

    ffmpeg

--- ffmpeg.py
+++ ffmpeg.py
@@ -1,10 +1,10 @@
+import asyncio
+import websockets
 import subprocess
-import cv2
-from flask import Flask, Response
 
 app = Flask(__name__)
 
-def generate_frames():
+def video_stream():
     # Use ffmpeg to capture video and stream it as MJPEG
     command = [
         'ffmpeg', 
@@ -16,32 +16,18 @@ def generate_frames():
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     while True:
-        frame = process.stdout.read(1024*1024)  # Read a chunk of MJPEG data
+        frame = process.stdout.read(1024)  # Read a chunk of MJPEG data
 
         if not frame:
+            print('no frame')
             continue
 
         yield (b'--frame\r\n'
                b'Content-Type: video/mjpeg\r\n\r\n' + frame + b'\r\n')
 
-@app.route('/')
-def index():
-    return '''
-    <!doctype html>
-    <html>
-    <head>
-        <title>Video Stream</title>
-    </head>
-    <body>
-        <h1>Video Stream</h1>
-        <img src="/video_feed" width="640" height="480">
-    </body>
-    </html>
-    '''
+# Start the WebSocket server
+async def main():
+    server = await websockets.serve(video_stream, '0.0.0.0', 8765)
+    await server.wait_closed()
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+asyncio.run(main())
