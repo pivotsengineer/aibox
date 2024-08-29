dcfa3598e594a416018bc72a55cc99c7e59801b1
Sergei Sergienko <pivotsengineer@pivotsdoo.com>
2024-08-29 23:22:02 +0200

    chunk_size = 1024 * 8

--- sockets.py
+++ sockets.py
@@ -11,7 +11,7 @@ camera_device = "/dev/media1"
 afterCheckTimeuot = 0.5
 aftercleanUpTimeuot = 0.3
 afterSendTimeuot = 0.25
-chunk_size = 1024 * 128
+chunk_size = 1024 * 8
 # basically the higher the number, the bigger the buffer array. 2 is a minimum # not sure here
 bufferMarker = 2
 # how many images in buffer
