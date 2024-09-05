1adde7205a064cc15f7fec9d748d3e3ac1b1bc65
Sergei Sergienko <pivotsengineer@pivotsdoo.com>
2024-09-05 16:19:54 +0200

    path

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO("yolov8n-cls.pt" )
-model = model(data="/Users/sergeysergienko/Library/Mobile Documents/com~apple~CloudDocs/Projects/newton/Dice-1", epochs=30)
+model = model(data="/home/sergienko/newton/Dice-1", epochs=30)
